ef7857a485ffc893455d5d27dd6b30166f4b93e6
Andrea Rabe <andrearaberabe@gmail.com>
2023-07-25 22:08:18 +0300

    database updated

--- database.py
+++ database.py
@@ -1,21 +1,19 @@
 import motor.motor_asyncio
+
 host = "cluster0.rusc6nw.mongodb.net"
-# port = 27017
 username = "devilnut28"
 password = "<password>"
 database_name = "app_mobile"
 
-# Create the "Connection URI"
-MONGODB_URI = f"mongodb+srv://{username}:{password}@{host}:/{database_name}?retryWrites=true&w=majority"
+# Créez la "Connection URI"
+MONGODB_URI = f"mongodb+srv://{username}:{password}@{host}/{database_name}?retryWrites=true&w=majority"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
 db = client[database_name]
-collection = db.collection("app_mobs")
-
 
 async def get_db():
-    db = await db.client().start_session()
+    db = client[database_name]
     try:
         yield db
     finally:
-        await db.close()
+        await client.close()
