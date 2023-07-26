1d2b6f8cfe5e378b809d0497b067fa859e808532
Andrea Rabe <andrearaberabe@gmail.com>
2023-07-26 21:02:26 +0300

    updated

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://freedb_flutter:9MH&ZSvj6z!YK8x@sql.freedb.tech:3306/freedb_app_mobile" # put here the name of your database here
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://freedb_rabe44:xpVn76&2KYK5CYv@sql.freedb.tech:3306/freedb_app_flutter" # put here the name of your database here
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
